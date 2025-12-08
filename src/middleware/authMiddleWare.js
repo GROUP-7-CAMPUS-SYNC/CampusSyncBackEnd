581d842331cad6e7ea3ea4da00a8045470158953
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-12-09 00:24:33 +0800

    a

--- src/middleware/authMiddleWare.js
+++ src/middleware/authMiddleWare.js
@@ -15,13 +15,10 @@ export const protect = async (request, response, next) => {
             token = request.headers.authorization.split(" ")[1];
             
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            console.log(decoded)
             request.userRegistrationDetails = await User.findById(decoded.userId).select("-password")
-            console.log(request.userRegistrationDetails)
             next();
         }catch(error)
         {
-            console.log(error);
             return response
                 .status(401)
                 .json({ message: "Not authorized, token failed" });
